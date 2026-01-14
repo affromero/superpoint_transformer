40142a86b29e6cd1b1b60b9a8e791e11801b5ff2
Andrés <me@afromero.co>
2026-01-14 05:35:03 +0000

    update install

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+set -e
 
 echo "⭐ Installing FRNN"
 
 # install a prefix_sum routine first
 cd src/dependencies/FRNN/external/prefix_sum
-pip install . --no-cache-dir --no-build-isolation # --force-reinstall
+rm -rf build
+python -m setup install
 
 # install FRNN
 cd ../../ # back to the {FRNN} directory
-pip install . --no-cache-dir --no-build-isolation # --force-reinstall
+rm -rf build
+python -m setup install
 cd ../../../
 
 # Compile the projects
